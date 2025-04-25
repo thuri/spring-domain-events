09b6ae374b587ee5672b0442a8d9f2d7d3d9cf05
Michael Lück <michael.lueck@rewe-group.com>
2025-04-25 08:50:27 +0200

    showing problem with preremove
    also events added during preremove aren't always handled correclty because the events may be added to the wrong instance.

--- src/main/kotlin/net/lueckonline/spring/domainevents/Application.kt
+++ src/main/kotlin/net/lueckonline/spring/domainevents/Application.kt
@@ -1,5 +1,6 @@
 package net.lueckonline.spring.domainevents
 
+import org.apache.logging.log4j.kotlin.logger
 import org.apache.logging.log4j.kotlin.loggerOf
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
@@ -15,10 +16,21 @@ fun main(args: Array<String>) {
 
     val context = runApplication<Application>(*args)
 
-    val exampleService = context.getBean(ExampleService::class.java)
-    logger.info("Running example")
-    exampleService.runExampleAsOneTransaction(Random.nextInt(), "Example 1")
-    logger.info("Ran example")
+    val service = context.getBean(ExampleService::class.java).apply {
+        logger.info("Running example")
+
+        logger.info("creating example")
+        val example1 = this.createExample(Random.nextInt(), "Test")
+        logger.info("Created example: $example1")
+
+        logger.info("deleting example: $example1")
+        this.deleteExample(example1)
+        logger.info("deleted example: $example1")
+
+        logger.info("Ran example")
+    }
+
+    service.runAsOnTransaction(Random.nextInt(), "Test 2")
 
 //    Thread.currentThread().join()
 }
--- src/main/kotlin/net/lueckonline/spring/domainevents/ExampleService.kt
+++ src/main/kotlin/net/lueckonline/spring/domainevents/ExampleService.kt
@@ -1,51 +1,61 @@
 package net.lueckonline.spring.domainevents
 
+import jakarta.persistence.EntityManager
 import org.apache.logging.log4j.kotlin.logger
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import kotlin.random.Random
 
 @Service
 class ExampleService (
-    @Autowired val repository: ExampleRepository
+    @Autowired val repository: ExampleRepository,
+    @Autowired val entityManager: EntityManager,
 ){
 
     @Transactional
-    fun runExampleAsOneTransaction(id: Int, name: String) {
+    fun runAsOnTransaction(id: Int, name: String) {
+        logger.info("Running example")
+
         logger.info("creating example")
-        val example1 = this.createExample(Random.nextInt(), "Test")
+        val example1 = this.createExample(id, name)
         logger.info("Created example: $example1")
+
         logger.info("deleting example: $example1")
         this.deleteExample(example1)
         logger.info("deleted example: $example1")
+
+        logger.info("Ran example")
     }
 
+//    @Transactional
     fun createExample(id: Int, name: String): ExampleEntity{
 
         val entityToSave = ExampleEntity(id, name)
         // if we don't call this directly but depend on the @PrePersist annotation
         // the created event is not evaluated during the save operation
-        //entityToSave.addCreatedEvent()
+        entityToSave.addCreatedEvent()
         logger.info("saving entity $entityToSave")
         val savedEntity = repository.saveAndFlush(entityToSave)
         logger.info("saved entity $savedEntity")
         return savedEntity
     }
 
+//    @Transactional(propagation = Propagation.REQUIRES_NEW)
     fun deleteExample(example: ExampleEntity) {
         logger.info("deleting entity")
+        logger.info("entitymanager contains entity to delete: ${entityManager.contains(example)}")
+//        example.addRemovedEvent()
         repository.delete(example)
         logger.info("deleted entity")
     }
 
-    @EventListener(classes = [ExampleCreated::class])
+    @EventListener
     fun handle(event: ExampleCreated){
         logger.info("Example created")
     }
 
-    @EventListener(classes = [ExampleRemoved::class])
+    @EventListener
     fun handle(event: ExampleRemoved){
         logger.info("Example removed")
     }
